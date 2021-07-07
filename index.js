3437a67e1a5a213828e64000e73fe10c23ba4504
Ajmol Uddin <uddin.ajmol@gmail.com>
2021-07-07 17:43:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 13
+let num4 = 4
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let multiply = num1 * num2;
+
+let mod = num4 % num3;
+
+let max = Math.max(Math.ceil(20));
